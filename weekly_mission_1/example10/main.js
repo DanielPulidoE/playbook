9147e077186e246c0f530d4620c70c856fd8d6ef
Daniel Reynaldo Pulido Estrada <99349487+DanielPulidoE@users.noreply.github.com>
2022-04-15 22:10:46 -0600

    Add files via upload

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,10 @@
+import MyPokemon from './pokemon.js'
+
+const pikachu = new Pokemon('pikachu')
+const bulbasaur = new Pokemon('bulbasaur')
+const squirtle = new Pokemon('squirtle')
+const charmander = new Pokemon('charmander')
+console.log(pikachu);
+console.log(bulbasaur);
+console.log(squirtle);
+console.log(charmander);
